0c071b26cd5ca301956a39aca259a2fc576f0bac
tofsir7 <khtofsir9191@gmail.com>
2026-04-26 22:32:14 +0600

    User and Product Schema Designed

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,2 +1,29 @@
 // I will add the Product model placeholder
-module.exports = {};
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    stock:{
+        type: Number,
+        required: true
+    },
+    expiryDate:{
+        type: Date,
+        required: true
+    },
+    barcode:{
+        type: String,
+        required: true,
+        unique: true
+    },
+  },
+);
+module.exports = mongoose.model("Product",productSchema);
