03695fa12e43b4f7ba5a5bc851e0b4d1d71cfac7
tofsir7 <khtofsir9191@gmail.com>
2026-04-26 15:04:49 +0600

    Database connected to MongoDB through Connection String

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
 // Database connection 
+const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         // I will connect to MongoDB here
-        console.log("Database connected Successfully");
+        conn = await mongoose.connect(process.env.CONNECTION_STRING);
+        console.log(`Database connected Successfully: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`Error found: ${error.message}`);
         process.exit(1);
     }
 };
